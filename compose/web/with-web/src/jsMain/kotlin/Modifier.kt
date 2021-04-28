8df7a40a229b88f058a8654cb44229553d7736ec
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2021-04-28 16:44:16 +0200

    Modifier.clickable - supported as of now in Text and Box

--- compose/web/with-web/src/jsMain/kotlin/Modifier.kt
+++ compose/web/with-web/src/jsMain/kotlin/Modifier.kt
@@ -8,6 +8,7 @@ import androidx.compose.web.css.px
 import androidx.compose.web.css.Color.RGB
 import org.jetbrains.compose.web.ww.internal.castOrCreate
 import androidx.compose.web.css.StyleBuilder
+import androidx.compose.web.attributes.AttrsBuilder
 
 actual fun Modifier.background(color: Color): Modifier = castOrCreate().apply {
     add {
@@ -16,15 +17,27 @@ actual fun Modifier.background(color: Color): Modifier = castOrCreate().apply {
 }
 
 fun Modifier.asStyleBuilderApplier(
-    followingHandler: (StyleBuilder.() -> Unit)? = null
+    passThroughHandler: (StyleBuilder.() -> Unit)? = null
 ): StyleBuilder.() -> Unit = castOrCreate().let { modifier ->
     val st: StyleBuilder.() -> Unit = {
         modifier.styleHandlers.forEach { it.invoke(this) }
-        followingHandler?.invoke(this)
+        passThroughHandler?.invoke(this)
     }
 
     st
 }
+
+fun Modifier.asAttributeBuilderApplier(
+    passThroughHandler: (AttrsBuilder<*>.() -> Unit)? = null
+): AttrsBuilder<*>.() -> Unit =
+    castOrCreate().let { modifier ->
+        val st: AttrsBuilder<*>.() -> Unit = {
+            modifier.attrHandlers.forEach { it.invoke(this) }
+            passThroughHandler?.invoke(this)
+        }
+
+        st
+    }
 
 actual fun Modifier.padding(all: Dp): Modifier = castOrCreate().apply {
     // yes, it's not a typo, what Modifier.padding does is actually adding marginEe
